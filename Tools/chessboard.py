a507729df3175ee9d5b87a087f4171d0d02b3450
jamespei <im7sea@gmail.com>
2016-03-10 18:52:40 +0800

    spider/

--- Tools/chessboard.py
+++ Tools/chessboard.py
@@ -13,16 +13,20 @@ def cover(board, lab=1, top=0, left=0, side=None):
     for dy_outer, dy_inner in offsets:
         for dx_outer, dx_inner in offsets:
             # if the  outer corner is not set...
+            print '1:',top+dy_outer,left+dx_outer
             if not board[top+dy_outer][left+dx_outer]:
                 #...label the inner corner:
+                print '2:',top+s+dy_inner,left+s+dx_inner
                 board[top+s+dy_inner][left+s+dx_inner] = lab
 
     # Next label:
     lab += 1
+    print 's:',s
     if s>1:
         for dy in [0,s]:
             for dx in [0,s]:
                 #recursive calls, if s is at least 2:
+                print 'lab:',lab,'  top:',top+dy,'  left:',left+dx
                 lab = cover(board, lab, top+dy, left+dx, s)
 
     # return the next available label:
@@ -33,4 +37,4 @@ board[7][7] = -1    # missing corner
 print cover(board)
 
 for row in board:
-    print(("%2i"*8) % tuple(row))
+    print(("%3i"*8) % tuple(row))
