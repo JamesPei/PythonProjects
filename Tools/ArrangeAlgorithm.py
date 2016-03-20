514b0338c28c30b2aed6b13c844ab3a81263fafb
JamesPei <im7sea@gmail.com>
2016-03-21 00:54:22 +0800

    20160321-1-0

--- Tools/ArrangeAlgorithm.py
+++ Tools/ArrangeAlgorithm.py
@@ -34,7 +34,7 @@ class ArrangeAlgorithms():
                 Q.append(j)
         return A
 
-    #排序算法
+    #通过键值函数进行排序的算法
     def counting_sort(self, A, key=lambda x:x):
         B,C = [], defaultdict(list)     #output and counts
         for x in A:
@@ -43,7 +43,7 @@ class ArrangeAlgorithms():
             B.extend(C[k])                    #add values in sorted order
         return B
 
-    #朴素版明星问题
+    #朴素版明星问题，时间复杂度O(n**2)
     def naive_celeb(self, G):
         n = len(G)
         for u in range(n):               #for every candidate
@@ -51,13 +51,40 @@ class ArrangeAlgorithms():
                 if u == v:  continue     #same person?Skip
                 if G[u][v]: break        #candidate knows other
                 if not G[v][u]: break    #other doesn't know candidate
-            else:
+            else:   #在for循环完整完成后才执行else；如果中途从break跳出，则连else一起跳出
                 return u                   #no breaks?celebrate
         return None                        #couldn't find anyone
 
-
+    #明星问题(即寻找人群中的一个人，他不认识人群中的其他人，但其他人都认识他),复杂度O(n)
+    def celeb(self, G):
+        n = len(G)
+        u,v = 0,1
+        for c in range(2, n+1):
+            if G[u][v]: u = c
+            else: v=c
+        if u==n: c=v
+        else: c=u
+        for v in range(n):
+            if c == v:continue
+            if G[c][v]:break
+            if not G[v][c]:break
+        else:
+            return c
+        return None
 
 M = [2,2,0,5,3,5,7,4]
 # print ArrangeAlgorithms().naive_max_perm(M)
 # print ArrangeAlgorithms().max_perm(M)
-print ArrangeAlgorithms().counting_sort(M)
+# print ArrangeAlgorithms().counting_sort(M)
+#明星算法验证，要想验证该算法就必须构建一个随机图
+from random import randrange
+n=100
+G=[[randrange(2) for i in range(n)] for i in range(n)]
+c=randrange(n)
+for i in range(n):
+    G[i][c]=True
+    G[c][i]=False
+print ArrangeAlgorithms().naive_celeb(G)
+print ArrangeAlgorithms().celeb(G)
+
+
---
+++ Tools/TopologyAlgorithms.py
@@ -0,0 +1,83 @@
+#__author__ = 'James'
+#-*-coding:utf-8-*-
+
+class Topology():
+
+    #朴素拓扑排序法，时间复杂度O(n**2)
+    def naive_topsort(self, G, S=None):
+        if S is None: S = set(G)
+        if len(S) == 1: return list(S)
+        v = S.pop()
+        seq = self.naive_topsort(G, S)
+        min_i = 0
+        for i, u in enumerate(seq):
+            if v in G[u]: min_i = i+1
+        seq.insert(min_i, v)
+        return seq
+
+    #有向无环图的拓扑排序,时间复杂度O(n)
+    def topsort(self, G):
+        count = dict((u, 0) for u in G)
+        for u in G:
+            for v in G[u]:
+                count[v] += 1
+        Q = [u for u in G if count[u] == 0]
+        S = []
+        while Q:
+            u = Q.pop()
+            S.append(u)
+            for v in G[u]:
+                count[v] -= 1
+                if count[v] == 0:
+                    Q.append(v)
+        return S
+
+    #indegree0函数返回入度为0的顶点，并在v和e中删除它和它相邻的边，如果v列表中没有顶点了，就返回None，
+    #如果v列表中还有顶点但是找不到入度为0的顶点，说明有向图中有环，返回-1。topoSort函数不断取出有向图中
+    #入度为0的顶点，最后就是拓扑排序序列
+    def indegree0(self,v,e):
+        if v==[]:
+            return None
+        tmp=v[:]
+        for i in e:
+            if i[1] in tmp:
+                tmp.remove(i[1])
+        if tmp==[]:
+            return -1
+
+        for t in tmp:
+            for i in range(len(e)):
+                if t in e[i]:
+                    e[i]='toDel' #占位，之后删掉
+        if e:
+            eset=set(e)
+            eset.remove('toDel')
+            e[:]=list(eset)
+        if v:
+            for t in tmp:
+                v.remove(t)
+        return tmp
+
+    def topoSort(self,v,e):
+        result=[]
+        while True:
+            nodes=self.indegree0(v,e)
+            if nodes==None:
+                break
+            if nodes==-1:
+                print('there\'s a circle.')
+                return None
+            result.extend(nodes)
+        return result
+
+# v=['a','b','c','d','e','f']
+# e=[('a','b'),('b','c'),('c','d'),('d','e'),('e','f'),('a','f'),('b','f'),('b','d'),('d','f')]
+v=['a','b','c','d','e']
+e=[('a','b'),('a','d'),('b','c'),('d','c'),('d','e'),('e','c')]
+res=Topology().topoSort(v,e)
+print(res)
+
+# G={'a':('b','f'),'b':('c','d','f'),'c':('d'),'d':('e','f'),'e':('f'),'f':()}
+G={'a':('b','d'),'b':('c'),'c':(),'d':('c','e'),'e':('c')}
+res=Topology().topsort(G)
+print res
