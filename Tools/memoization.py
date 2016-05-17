45029b0e804e4141929219109f49a420e4884889
JamesPei <im7sea@gmail.com>
2016-05-17 08:28:52 +0800

    20160517-1-0

--- Tools/memoization.py
+++ Tools/memoization.py
@@ -75,7 +75,17 @@ def rec_lcs(a,b):
         return max(L(i-1,j),L(i,j-1))
     return L(len(a)-1, len(b)-1)
 
-
+def lcs(a,b):
+    n,m = len(a),len(b)
+    pre,cur=[0]*(n+1),[0]*(n+1)
+    for j in range(1,m+1):
+        pre,cur=cur,pre
+        for i in range(1,n+1):
+            if a[i-1]==b[j-1]:
+                cur[i]=pre[i-1]+1
+            else:
+                cur[i]=max(pre[i], cur[i-1])
+    return cur[n]
 
 if __name__=='__main__':
     # print fib(100)  #指数级耗时过长或无法计算
@@ -89,4 +99,8 @@ if __name__=='__main__':
     # print dag_sp(dag, 'a', 'f')
 
     seq=[1,2,3,6,8,0,4,5,11,32,12]
-    print lis(seq)
+    # print lis(seq)
+
+    a,b='spock','asoka'
+    # print rec_lcs(a,b)
+    print lcs(a,b)
