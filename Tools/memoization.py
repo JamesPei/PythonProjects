826aa16191e1a4cd36172bf128b1a8fb09ba30c1
jamespei <im7sea@gmail.com>
2016-05-21 16:53:22 +0800

    201605221-1-0

--- Tools/memoization.py
+++ Tools/memoization.py
@@ -87,6 +87,32 @@ def lcs(a,b):
                 cur[i]=max(pre[i], cur[i-1])
     return cur[n]
 
+# 用于实现最优搜索树的记忆体化递归函数
+def rec_opt_tree(p):
+    @memo
+    def s(i,j):
+        if i==j: return 0
+        return s(i,j-1)+p[j-1]
+    @memo
+    def e(i,j):
+        if i==j: return 0
+        sub = min(e(i,r)+e(r+1,j) for r in range(i,j))
+        return sub + s(i,j)
+    return e(0,len(p))
+
+# 用迭代方式解决最优搜索树问题
+from collections import defaultdict
+def opt_tree(p):
+    n = len(p)
+    s,e = defaultdict(int), defaultdict(int)
+    for k in range(1,n+1):
+        for i in range(n-k+1):
+            j = i+k
+            s[i,j] = s[i,j-1] + p[j-1]
+            e[i,j] = min(e[i,r]+e[r+1,j] for r in range(i,j))
+            e[i,j] += s[i,j]
+    return e[0,n]
+
 if __name__=='__main__':
     # print fib(100)  #指数级耗时过长或无法计算
 
