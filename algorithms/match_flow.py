49d15136a1a964c5576a7e79a3604eb34916089d
jamespei <lei.pei@xtalpi.com>
2016-06-06 19:14:41 +0800

    20160606-0-0

--- algorithms/match_flow.py
+++ algorithms/match_flow.py
@@ -11,32 +11,6 @@ def tr(G):
             GT[v].add(u)
     return GT
 
-# 通过增广路径算法来寻找双边最大匹配
-def match(G,X,Y):                                           # maximum bipartite matching
-    H = tr(G)                                               # the transposed graph
-    S,T,M = set(X), set(Y), set()                           # unmatched left/right + match
-    while S:                                                # still unmatched on the left
-        s = S.pop()                                         # get one
-        Q,P = {s},{}                                        # start a traversal from it
-        while Q:                                            # discovered, unvisited
-            u = Q.pop()                                     # visit one
-            if u in T:                                      # Finished augmenting path?
-                T.remove(u)                                 # u is now matched
-                break                                       # and our traversal is done
-            forw = (v for v in G[u] if (u,v) not in M)      # possible new edges
-            back = (v for v in H[u] if (v,u) in M)          # cancellations
-            for v in chain(forw, back):                     # along out- and in-edges
-                if v in P: continue                         # already visited? Ignore
-                P[v] = u                                    # traversal predecessor
-                Q.add(v)                                    # new nodel discovered
-        while u != s:                                   # augment: backtrack to s
-            u, v = P[u], u                              # shift one step
-            if v in G[u]:                               # forward edges?
-                M.add((u,v))                            # new edge
-            else:                                       # backward edge?
-                M.remove((v,u))                         # cancellation
-    return M                                                # matching--a set of edges
-
 # 使用带标记的遍历来寻找增广路径，并对边不相交的路径进行计数
 def paths(G, s, t):                                         # edge-disjoint path count
     H, M, count = tr(G), set(), 0                           # transpose, matching, result
@@ -63,18 +37,9 @@ def paths(G, s, t):                                         # edge-disjoint path
                 M.remove((v, u))                            # cancellation
 
 if __name__=='__main__':
-    G={'A':('1','3'), 'B':('2','4'), 'C':('1','4'), 'D':'3', '1':('A','C'), '2':'B', '3':('A','D'), '4':('B','C')}
-    X=['A','B','C','D']
-    Y=['1','2','3','4']
-    # G={'A':('1','3'), 'B':('2','4'), 'C':('1','4','5'), 'D':'3', 'E':'4', '1':('A','C'), '2':'B', '3':('A','D'), '4':('B','C','E'), '5':'C'}
-    # X=['A','B','C','D','E']
-    # Y=['1','2','3','4','5']
-    M=match(G,X,Y)
-    print M
-
-    # G={'s':('a','c','e'),'a':'b','c':('b','d','f'), 'e':'f', 'b':'t', 'd':'t', 'f':'t', 't':''}
-    # G={'s':('a','c'),'a':'b','c':('b','d'), 'b':'t', 'd':'t', 't':''}
-    # s='s'
-    # t='t'
-    # print paths(G,s,t)
+    G={'s':('a','c','e'),'a':'b','c':('b','d','f'), 'e':'f', 'b':'t', 'd':'t', 'f':'t', 't':''}
+    G={'s':('a','c'),'a':'b','c':('b','d'), 'b':'t', 'd':'t', 't':''}
+    s='s'
+    t='t'
+    print paths(G,s,t)
     
