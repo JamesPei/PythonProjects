4b345031f468556e063abbd7878fee1f9cec23a2
jamespei <lei.pei@xtalpi.com>
2016-06-29 18:42:50 +0800

    20160629-0-0

--- algorithms/hungary.py
+++ algorithms/hungary.py
@@ -39,6 +39,56 @@ class DFS_hungary():
                         return 1
         return 0
 
+def BFS_hungary():
+    # g=[[1,0,1,0],[0,1,0,1],[1,0,0,1],[0,0,1,0]]
+    # Nx=4
+    # Ny=4
+    # Mx=[-1,-1,-1,-1]
+    # My=[-1,-1,-1,-1]
+    # chk=[-1,-1,-1,-1]
+    # Q=[0,0,0,0]
+    # prev=[0,0,0,0]
+    g=[[0,1,0],[0,1,1],[1,0,0]]
+    Nx=3
+    Ny=3
+    Mx=[-1,-1,-1]
+    My=[-1,-1,-1]
+    chk=[-1,-1,-1]
+    Q=[0,0,0]
+    prev=[0,0,0]
+    res=0
+    for i in xrange(Nx):
+        if Mx[i]==-1:
+            qs=qe=0
+            Q[qe]=i
+            qe+=1
+            prev[i]=-1
+
+            flag=0
+            while(qs<qe and not flag):
+                u=Q[qs]
+                for v in xrange(Ny):
+                    if flag:continue
+                    if g[u][v] and chk[v]!=i:
+                        chk[v]=i
+                        Q[qe]=My[v]
+                        qe+=1
+                        if My[v]>=0:
+                            prev[My[v]]=u
+                        else:
+                            flag=1
+                            d,e=u,v
+                            while d!=-1:
+                                t=Mx[d]
+                                Mx[d]=e
+                                My[e]=d
+                                d=prev[d]
+                                e=t
+                qs+=1
+            if Mx[i]!=-1:
+                res+=1
+    return res
+
 if __name__ == '__main__':
     # nx, ny=['A','B','C'] ,['E','F','G']
     # edge={'A':{'E':1,'F':1,'G':0}, 'B':{'E':0, 'F':0, 'G':1}, 'C':{'E':1, 'F':0, 'G':1}}
@@ -50,4 +100,5 @@ if __name__ == '__main__':
     visited = {'E': 0, 'F': 0, 'G': 0,'H':0}
 
     # dfs算法寻找增广路，常用在稠密图中，算法的复杂度为O(n3)
-    print DFS_hungary(nx, ny, edge, cx, cy, visited).max_match()
+    # print DFS_hungary(nx, ny, edge, cx, cy, visited).max_match()
+    print BFS_hungary()
