93480b5f2dc70df5ebb869098a435fa0100d3b4d
jamespei <lei.pei@xtalpi.com>
2016-07-05 15:51:56 +0800

    201607005-0-0

--- algorithms/TopologyAlgorithms.py
+++ algorithms/TopologyAlgorithms.py
@@ -30,6 +30,36 @@ def topsort(G):
                 Q.append(v)
     return S
 
+#具有分层功能的拓扑排序
+def toposort_group(G):
+    count = dict((u, 0) for u in G)
+    for u in G:
+        for v in G[u]:
+            count[v] += 1
+    G1=deepcopy(G)
+    Q=deque([u for u in G if count[u] == 0])    # 初始节点
+    S = []
+    pre = [u for u in G if count[u] == 0]       # 上一层节点
+    t=[]
+    tails=[]                                    # 最后一层节点
+    while Q:
+        if not pre:
+            t.append(deepcopy(S))
+            S=[]
+            pre = [u for u in G1 if count[u] == 0]
+        u = Q.popleft()
+        pre.remove(u)
+        S.append(u)
+        if not G[u]:                            # 不存在子节点
+            tails.append(u)
+        for v in G[u]:
+            count[v] -= 1
+            if count[v] == 0:
+                Q.append(v)
+        if u in G1.keys():G1.pop(u)
+    t.append(tails)
+    return t
+
 #indegree0函数返回入度为0的顶点，并在v和e中删除它和它相邻的边，如果v列表中没有顶点了，就返回None，
 #如果v列表中还有顶点但是找不到入度为0的顶点，说明有向图中有环，返回-1。topoSort函数不断取出有向图中
 #入度为0的顶点，最后就是拓扑排序序列
